498169699800815c9b501ce34c0354cfbdacb900
Johnfg10 <johnfg2610@gmail.com>
2017-11-09 18:59:53 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Download_Cleaner
+{
+    class Program
+    {
+        public static readonly string downloadfolder = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + "Downloads";
+        public static readonly DirectoryInfo downloadFolder = Directory.CreateDirectory(downloadfolder);
+        public static readonly string startupfolder = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        public static readonly string startupLocation = startupfolder + Path.DirectorySeparatorChar + "download_cleaner.exe";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(startupLocation);
+            Console.WriteLine("Clearing downloads folder!");
+            if (!File.Exists(startupLocation))
+            {
+                Console.WriteLine("Creating statup!");
+                File.Copy(Assembly.GetExecutingAssembly().Location, startupLocation);
+            }
+            Empty(downloadFolder);
+        }
+
+        public static void Empty(DirectoryInfo directory)
+        {
+            foreach (FileInfo file in directory.GetFiles()) file.Delete();
+            foreach (DirectoryInfo subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+        }
+    }
+}
